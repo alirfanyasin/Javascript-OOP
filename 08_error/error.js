d7db74d73fb4fbaa1c04ae91e342859d58d79a4b
alirfanyasin <risayunita21@gmail.com>
2024-02-15 11:53:57 +0700

    error

---
+++ 08_error/error.js
@@ -0,0 +1,20 @@
+// ERROR
+
+class MathUtil {
+
+  static sum(...numbers){
+    if(numbers.length === 0){
+      throw new Error('Total parameter harus lebih dari 0')
+    }
+
+    let total = 0;
+    for (const number of numbers) {
+      total += number
+    }
+    return total
+  }
+
+}
+
+console.log(MathUtil.sum())
+console.log(MathUtil.sum(1,2,3,4,5))
